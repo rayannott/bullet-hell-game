40af56bda1f7fb357684c988fab53abe6c57a3c5
Airat <imjobjob@gmail.com>
2023-11-30 15:44:22 +0100

    added timer

--- src/game.py
+++ src/game.py
@@ -1,19 +1,23 @@
 from collections import defaultdict
 
 from base import Entity, EntityType
-from config.config import REMOVE_DEAD_ENTITIES_EVERY
+from config.back import REMOVE_DEAD_ENTITIES_EVERY
+from utils import Timer
 
 class Game:
     def __init__(self) -> None:
         self.entities: defaultdict[EntityType, list[Entity]] = defaultdict(list)
+        self.remove_dead_entities_timer = Timer(max_time=REMOVE_DEAD_ENTITIES_EVERY)
 
-    def update(self) -> None:
-        pass
+    def update(self, time_delta: float) -> None:
+        self.remove_dead_entities_timer.tick(time_delta)
+        if not self.remove_dead_entities_timer.running():
+            self.remove_dead_entities()
 
     def remove_dead_entities(self):
         """
         Remove all dead entities.
-        This is called every REMOVE_DEAD_ENTITIES_EVERY game ticks
+        This function is called every REMOVE_DEAD_ENTITIES_EVERY seconds.
         """
         new_entities = defaultdict(list)
         for entity_type, entities in self.entities.items():
@@ -21,3 +25,4 @@ class Game:
                 if entity.is_alive():
                     new_entities[entity_type].append(entity)
         self.entities = new_entities
+        self.remove_dead_entities_timer.reset()
--- src/utils.py
+++ src/utils.py
@@ -25,3 +25,27 @@ class Slider[T: int | float]:
     
     def __str__(self) -> str:
         return f'Slider({self.current_value:.1f}/{self.max_value:.1f})'
+
+
+class Timer:
+    """
+    Counts time in seconds.
+    """
+    def __init__(self, max_time: float):
+        self.max_time = max_time
+        self.current_time = 0.
+    
+    def tick(self, time_delta: float) -> None:
+        self.current_time += time_delta 
+
+    def running(self) -> bool:
+        return self.current_time < self.max_time
+    
+    def get_current(self) -> float:
+        return self.current_time
+    
+    def reset(self) -> None:
+        self.current_time = 0.
+    
+    def __str__(self) -> str:
+        return f'Timer({self.current_time:.1f}/{self.max_time:.1f})'
