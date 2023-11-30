e5034c765d92abae78f53cb785f7828c13c013a9
Airat <imjobjob@gmail.com>
2023-11-30 15:54:31 +0100

    Update player speed range and projectile default speed

--- config/back.py
+++ config/back.py
@@ -3,12 +3,12 @@ TRAIL_MAX_LENGTH = 50 # positions
 
 # player
 PLAYER_SIZE = 30.
-PLAYER_SPEED_RANGE = (0.8, 10.0) # min, max: depends on the gravity point
+PLAYER_SPEED_RANGE = (50., 500.) # min, max: depends on the distance to the gravity point
 PLAYER_MAX_HEALTH = 100.
 PLAYER_MAX_ENERGY = 1000
 PLAYER_STARTING_ENERGY = PLAYER_MAX_ENERGY // 10
 
 # projectile
 PROJECTILE_DEFAULT_SIZE = 8.
-PROJECTILE_DEFAULT_SPEED = 5.
+PROJECTILE_DEFAULT_SPEED = 250. # pixels per second
 PROJECTILE_DEFAULT_LIFETIME = 10. # seconds
