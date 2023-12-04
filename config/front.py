b0d52153adc2ce9e0101780b0cccc6260ad24d4c
Airat <imjobjob@gmail.com>
2023-12-04 14:36:59 +0100

    added pathlib directory constants

--- config/front.py
+++ config/front.py
@@ -1,3 +1,6 @@
+import pathlib
+
+
 FRAMERATE = 60
 
 SM = 5 # small margin
@@ -14,3 +17,7 @@ GAME_HEALTH_BAR_SIZE = 200, 50
 GAME_ENERGY_BAR_SIZE = 200, 25
 
 GAME_STATS_TEXTBOX_SIZE = 200, 200
+
+ASSETS_DIR = pathlib.Path('assets').resolve()
+SOUNDS_DIR = ASSETS_DIR / 'sounds'
+SAVES_DIR = ASSETS_DIR / 'saves'
