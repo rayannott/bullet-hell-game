636e86840349d2af0dc4ab5e36f91211242fd952
Airat <imjobjob@gmail.com>
2023-12-04 14:37:38 +0100

    added playing through the sound effects in main

--- main.py
+++ main.py
@@ -10,5 +10,14 @@ def main():
     menu_screen.run()
 
 
+def play_through_sound_effects():
+    from front.sounds import play_sfx, SOUND_EFFECTS
+    for k in SOUND_EFFECTS:
+        play_sfx(k)
+        print(k)
+        input()
+
+
 if __name__ == '__main__':
     main()
+    # play_through_sound_effects()
