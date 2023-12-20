dd72d453eb6f5862e9475c0f5df643a50c711df8
Airat <airat.valiullin@tum.de>
2023-12-20 11:15:40 +0100

    stats window

--- front/stats_window.py
+++ front/stats_window.py
@@ -41,33 +41,31 @@ class StatsWindow(pygame_gui.windows.UIMessageWindow):
     
     def construct_one_save_html(self, datetime_str: str, info: dict) -> str:
         # TODO
-        difficulty_field =  f"{paint('difficulty', PRETTY_MAGENTA)}: {paint(info['difficulty'], NICER_GREEN)};" if info['difficulty'] != 3 else ''
+        TAB = '    '
+        difficulty_field =  f" {paint('difficulty', PRETTY_MAGENTA)}: {paint(info['difficulty'], NICER_GREEN)};" if info['difficulty'] != 3 else ''
         died_because_str = f"{paint('died because', PRETTY_MAGENTA)} {paint(info['reason_of_death'], NICER_RED)}" if info['reason_of_death'] else paint('did not die', PRETTY_MAGENTA)
         achievements_list = info['achievements'].achievements_pretty()
-        achievements_str = f'{paint(", ".join(achievements_list), NICER_BLUE)}' if achievements_list else 'none'
+        achievements_str = f'{paint('achievements', PRETTY_MAGENTA)}:\n{TAB*2}{paint(", ".join(achievements_list), NICER_BLUE)}' if achievements_list else ''
         artifacts_handler: ArtifactsHandler = info['artifacts']
 
         stats_boosts = [el.stats_boost for el in artifacts_handler.inactive_artifacts]
         total_stats_boots = sum(stats_boosts, StatsBoost())
-        stats_boosts_str = f'{paint(", ".join(map(str, stats_boosts)), NICER_YELLOW)}' if stats_boosts else 'none'
+        stats_boosts_str = f'stat boosts:\n{TAB*3}{paint(", ".join(map(str, stats_boosts)), NICER_YELLOW)}\n{TAB*3}(total: {paint(str(total_stats_boots), NICER_YELLOW)})' if stats_boosts else ''
         active_artifacts = [el.get_verbose_string() for el in artifacts_handler.iterate_active()]
-        active_artifacts_str = f'{paint(", ".join(active_artifacts), LIGHT_ORANGE)}' if active_artifacts else 'none'
+        active_artifacts_str = f'active:\n{TAB*3}{paint(", ".join(active_artifacts), LIGHT_ORANGE)}' if active_artifacts else ''
 
-        stats_str = info['stats'].get_pretty_stats() # TODO: formatting
+        stats_list_pairs: list[tuple[str, str]] = info['stats'].get_pretty_stats() # TODO: formatting
+        stats_str = '\n        '.join(f'{paint(k, NICER_GREEN):<60} {paint(v, NICER_YELLOW):>5}' for k, v in stats_list_pairs)
+        
+        artifacts_str = f'{paint('artifacts', PRETTY_MAGENTA)}:\n{TAB*2}{stats_boosts_str}\n{TAB*2}{active_artifacts_str}' if stats_boosts_str or active_artifacts_str else ''
 
-
-        text = f'''<b>--- {paint('GAME', WHITE)} {paint(datetime_str.replace('/', '.'), LIGHT_BLUE)} ---</b>
+        DASHES = '-' * 10
+        text = f'''<b>{DASHES} {paint('GAME', WHITE)} {paint(datetime_str.replace('/', '.'), LIGHT_BLUE)} {DASHES}</b>
     {paint('level', PRETTY_MAGENTA)}: {paint(info['level'], NICER_GREEN)};{difficulty_field} {paint('time', PRETTY_MAGENTA)}: {paint('{:.2f}'.format(info['time']), NICER_GREEN)} sec
     {paint('stats', PRETTY_MAGENTA)}:
         {stats_str}
-    {paint('achievements', PRETTY_MAGENTA)}:
-        {achievements_str}
-    {paint('artifacts', PRETTY_MAGENTA)}:
-        stats:
-            {stats_boosts_str}
-            (total: {paint(str(total_stats_boots), NICER_YELLOW)})
-        active:
-            {active_artifacts_str}
+    {achievements_str}
+    {artifacts_str}
     {died_because_str}
     '''
         return text
